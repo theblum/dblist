5c6dcac385fbb8ff864f09ee4418f58d9ca2de73
Brian Blumberg <blum@disroot.org>
2018-09-26 09:08:55 -0700

    Remove duplicate options.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CFLAGS="-std=c++17 -g -O0 -Wall -Wextra -pedantic -pipe"
+CFLAGS="-std=c++17 -g -O0 -pedantic -pipe"
 WARNINGS="-Wall -Wextra -Wno-unused-variable -Wno-unused-parameter -Wno-unused-function"
 LDLIBS=""
 time clang++ $CFLAGS $WARNINGS $@ -o dblist dblist.cpp $LDLIBS
